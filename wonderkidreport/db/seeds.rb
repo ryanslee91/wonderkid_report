2a5f3756537aaa155cb13fa3e44986137de2d872
Sang Hyun Lee <you@example.com>
2021-08-26 15:34:29 -0400

    backend seeds building

--- wonderkidreport/db/seeds.rb
+++ wonderkidreport/db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Position.destroy_all
+Player.destroy_all
+User.destroy_all
+
+@admin = User.create!(username: 'RyanL', email: 'ryanlee@email.com', password: '123456')
+@josem = User.create!(username: 'JoseM', email: 'josem@email.com', password: '123456')
+@alexf = User.create!(username: 'AlexF', email: 'alexf@email.com', password: '123456')
+@pepg = User.create!(username: 'PepG', email: 'pepg@email.com', password: '123456')
+@juliann = User.create!(username: 'JulianN' email: 'juliann@email.com', password: '123456')
+@arsenew = User.create!(username: 'ArseneW' email: 'arsenew@email.com', password: '123456')
+
+puts "#{User.count} users created"
+
+Player.create!(name: )
+
+@forward = Position.create!(name: 'forward')
+@midfielder = Position.create!(name: 'midfielder')
+@defender = Position.create!(name: 'defender')
+@goalkeeper = Position.create!(name: 'goalkeeper')
+
+puts "#{Position.count} positions created"
+
+
