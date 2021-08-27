e852adb1c6ce8bbeff27ba714508f630689e7871
Sang Hyun Lee <you@example.com>
2021-08-27 15:54:40 -0400

    layout build fe

---
+++ wonderkidreport/client/src/layouts/Layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+export default function Layout() {
+  const {currentUser, handleLogout} = props
+  return (
+    <div>
+      
+    </div>
+  )
+}
