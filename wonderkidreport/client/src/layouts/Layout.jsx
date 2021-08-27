541ebdf2d5eab27a96a56347fc6ebf4e02a33ed7
Sang Hyun Lee <you@example.com>
2021-08-27 16:07:40 -0400

    layout and app setup

--- wonderkidreport/client/src/layouts/Layout.jsx
+++ wonderkidreport/client/src/layouts/Layout.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-export default function Layout() {
+export default function Layout(props) {
   const {currentUser, handleLogout} = props
   return (
     <div>
-      
+      <header>
+        <h1>Wonderkid Report</h1>
+        {
+          currentUser ? (
+            <div>
+              <p>
+                {currentUser.username}
+                <button onClick={handleLogout}>Logout</button>
+              </p>
+              </div>
+          ) : (
+              <Link to='/login'>Login/Register</Link>
+          )
+        }
+        <hr />
+        {currentUser && (
+          <div>
+            <Link to='/leagues'>
+              Leagues
+              </Link>
+          </div>
+        )}
+        {props.children}
+      </header>
     </div>
   )
 }
