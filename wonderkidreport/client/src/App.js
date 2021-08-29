c83d1faf52adb084270334c697e12af2447edbe9
Sang Hyun Lee <you@example.com>
2021-08-28 20:27:00 -0400

    fe app login setup

--- wonderkidreport/client/src/App.js
+++ wonderkidreport/client/src/App.js
@@ -14,14 +14,34 @@ function App() {
     handleVerify();
   }, []);
 
+  const handleLogin = async (loginData) => {
+    const userData = await loginUser(loginData);
+    setCurrentUser(userData);
+    history.push('/');
+  };
+
+  const handleRegister = async (registerData) => {
+    const userData = await registerUser(registerData);
+    setCurrentUser(userData);
+    history.push('/');
+  };
+  
+  const handleLogout = () => {
+    setCurrentUser(null);
+    localStorage.removeItem('authToken');
+    removeToken();
+    history.push('/');
+  };
 
   return (
     <div className="App">
-      <Layout currentUser={currentUser}>
+      <Layout currentUser={currentUser} handleLogout={handleLogout}>
         <Switch>
-
+          <Route path='/login'>
+            <Login handleLogin={handleLogin} />
+          </Route>
         </Switch>
-  </Layout>
+      </Layout>
     </div>
   );
 }
