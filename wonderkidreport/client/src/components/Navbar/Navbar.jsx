8ace1edb4b834f0d3305d1c46575d62000f472cb
Sang Hyun Lee <you@example.com>
2021-08-28 20:15:47 -0400

    fe navbar update

--- wonderkidreport/client/src/components/Navbar/Navbar.jsx
+++ wonderkidreport/client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function Navbar() {
+export default function Navbar(props) {
+  const { currentUser, handleLogout } = props;
   return (
     <div>
-      <h1>Navbar</h1>
+      <header>
+        <h1>Wonderkid Report</h1>
+        {currentUser ? (
+          <div>
+            <p>{currentUser.username}</p>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        ) : (
+   <Link to='/login'>Login/Register</Link>
+        )}
+        {currentUser && (
+          <div>
+            
+          </div>
+        )}
+          {props.children}
+        </header>
     </div>
   )
 }
