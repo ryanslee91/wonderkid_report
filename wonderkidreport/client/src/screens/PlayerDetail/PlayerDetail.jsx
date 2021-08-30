de7a57f18a2cff658bea3b0842e1451533b046f1
Sang Hyun Lee <you@example.com>
2021-08-30 15:37:23 -0400

    detail page setting up

--- wonderkidreport/client/src/screens/PlayerDetail/PlayerDetail.jsx
+++ wonderkidreport/client/src/screens/PlayerDetail/PlayerDetail.jsx
@@ -1,3 +1,23 @@
 import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { getOnePlayer } from "../../services/players";
+
+export default function PlayerDetail(props) {
+  const [player, setPlayer] = useStaet(null);
+  const { currentUser, players } = props;
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchPlayer = async () => {
+      const player = await getOnePlayer()
+      setPlayer(player)
+    }
+    fetchPlayer()
+  }, [id, players]);
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
