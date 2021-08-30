7a56df265b08b79435627b2260c6bf04c80d2126
Sang Hyun Lee <you@example.com>
2021-08-30 14:59:29 -0400

    fe tabbar setup

--- wonderkidreport/client/src/screens/Home/Home.jsx
+++ wonderkidreport/client/src/screens/Home/Home.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import PlayerCard from '../../components/PlayerCard/PlayerCard'
+import Tabbar from '../../components/Tabbar/Tabbar'
 
 
 export default function Home(props) {
   return (
     <div>
-   
-<PlayerCard />
-
+   <Tabbar />
     </div>
   )
 }
